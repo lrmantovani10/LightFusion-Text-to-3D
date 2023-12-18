36371a0a068d72644390a9dd0a1151012df1fb2b
Lucas Mantovani <lrmantovani@Lucass-MacBook-Air-2.local>
2023-12-18 15:51:31 -0600

    sd

--- sd.py
+++ sd.py
@@ -7,36 +7,20 @@ import util
 base_model = "stabilityai/stable-diffusion-xl-base-1.0"
 refiner_model = "stabilityai/stable-diffusion-xl-refiner-1.0"
 
-# Fast mode - slightly lower quality tradeoff
-fast_mode = True
 
-# Check if CUDA is available
-cuda_there = False
-# Check if MPS is available
-mps_there = False
-if torch.cuda.is_available():
-    # If there are any problems installing deepspeed, run "pip install py-cpuinfo" before installing deepspeed
-    import deepspeed
+def build_pipes(device):
+    pipe_base = StableDiffusionXLPipeline.from_pretrained(
+        base_model,
+        torch_dtype=torch.float32,
+        revision="main",
+    )
 
-    cuda_there = True
+    pipe_refiner = StableDiffusionXLPipeline.from_pretrained(
+        refiner_model,
+        torch_dtype=torch.float32,
+        revision="main",
+    )
 
-# For MPS to work, you must (as of this writing) use the PyTorch nightly build, so make sure that is the one you have installed for your virtual env
-elif torch.backends.mps.is_available():
-    mps_there = True
-
-pipe_base = StableDiffusionXLPipeline.from_pretrained(
-    base_model,
-    torch_dtype=torch.float32,
-    revision="main",
-)
-
-pipe_refiner = StableDiffusionXLPipeline.from_pretrained(
-    refiner_model,
-    torch_dtype=torch.float32,
-    revision="main",
-)
-
-if fast_mode:
     for pipe in [pipe_base, pipe_refiner]:
         pipe.enable_attention_slicing()
         # Apply Token merging with a 50% merging ratio - this speeds up the model by ~ 2x
@@ -44,7 +28,11 @@ if fast_mode:
 
         # Check if Cuda is available before using deepspeed
         # Here, we only use deepspeed because it's a powerful speedup already, but we can optimize things further by creating a BATCH file that runs the code by calling Meta's xformers - https://github.com/facebookresearch/xformers
-        if cuda_there:
+        if device == "cuda":
+            # If there are any problems installing deepspeed, run "pip install py-cpuinfo" before installing deepspeed
+            # For MPS to work, you must (as of this writing) use the PyTorch nightly build, so make sure that is the one you have installed for your virtual env
+            import deepspeed
+
             with torch.inference_mode():
                 deepspeed.init_inference(
                     model=getattr(pipe, "model", pipe),
@@ -54,98 +42,108 @@ if fast_mode:
                     replace_with_kernel_inject=False,  # Whether to replace the model with the kernel injector
                 )
 
-for pipe in [pipe_base, pipe_refiner]:
-    # Scheduler
-    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+        # Scheduler
+        pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 
-    # where to pipe?
-    if cuda_there:
-        print("Using CUDA")
-        pipe = pipe.to("cuda")
-    elif mps_there:
-        print("Using MPS")
-        # Uncomment this to use MPS on a Mac instead of CPU (may run into memory shortage issues, but if there is enough memory that can be allocated, will be much faster than CPU)
-        # pipe = pipe.to("mps")
-    else:
-        print("Using CPU")
-        pipe.to("cpu")
+        # where to pipe?
+        if device == "cuda":
+            print("Using CUDA")
+            pipe = pipe.to("cuda")
+        elif device == "mps":
+            print("Using MPS")
+            # Uncomment this to use MPS on a Mac instead of CPU (may run into memory shortage issues, but if there is enough memory that can be allocated, will be much faster than CPU)
+            # pipe = pipe.to("mps")
+        else:
+            print("Using CPU")
+            pipe.to("cpu")
+
+    return pipe_base, pipe_refiner
+
 
 # Number of inference steps
 num_steps = 100
 
 
-# Function to generate the intrinsics.txt matrix from the image
-def generate_intrinsics(view_scale, fovy, width, height):
-    H = int(view_scale * height)
-    W = int(view_scale * width)
-    cx = H / 2
-    cy = W / 2
-    focal = H / (2 * np.tan(np.deg2rad(fovy) / 2))
-    intrinsics = np.array([focal, focal, cx, cy])
-    return intrinsics
+# Function to generate the camera intrinsics parameters
+def generate_intrinsics(fovy, width, height):
+    """
+    The camera intrinsics are represented in the following notation:
+    (line 1) f (or fx), cx, cy
+    (line 2) height, width
+    """
+    cx = width / 2
+    cy = height / 2
+    focal = width / (2 * np.tan(np.deg2rad(fovy) / 2))
+    intrinsics = np.array([focal, cx, cy])
+    image_intrinsics = " ".join([str(num) for num in intrinsics])
+    image_intrinsics += str(height) + " " + str(width) + "\n"
+    image_intrinsics = image_intrinsics.encode("utf-8")
+    return image_intrinsics
 
 
-# Convert degrees to radians
-def deg_to_rad(deg):
-    return deg * np.pi / 180
+def rotation_matrix_from_euler(roll, pitch, yaw):
+    """
+    Create a rotation matrix from Euler angles (roll, pitch, yaw).
 
-
-# Define the rotation matrices for rotations around the X and Y axes
-def rotation_x(angle):
-    rad = deg_to_rad(angle)
-    return np.array(
-        [[1, 0, 0], [0, np.cos(rad), -np.sin(rad)], [0, np.sin(rad), np.cos(rad)]]
+    :param roll: Rotation angle around the x-axis in radians.
+    :param pitch: Rotation angle around the y-axis in radians.
+    :param yaw: Rotation angle around the z-axis in radians.
+    :return: A 3x3 rotation matrix.
+    """
+    R_x = np.array(
+        [[1, 0, 0], [0, np.cos(roll), -np.sin(roll)], [0, np.sin(roll), np.cos(roll)]]
     )
 
-
-def rotation_y(angle):
-    rad = deg_to_rad(angle)
-    return np.array(
-        [[np.cos(rad), 0, np.sin(rad)], [0, 1, 0], [-np.sin(rad), 0, np.cos(rad)]]
+    R_y = np.array(
+        [
+            [np.cos(pitch), 0, np.sin(pitch)],
+            [0, 1, 0],
+            [-np.sin(pitch), 0, np.cos(pitch)],
+        ]
     )
 
+    R_z = np.array(
+        [[np.cos(yaw), -np.sin(yaw), 0], [np.sin(yaw), np.cos(yaw), 0], [0, 0, 1]]
+    )
 
-# Function to generate pose
-def generate_pose_data(rotation):
-    # Image rotations
-    x_rotation = rotation_x(rotation[0])
-    y_rotation = rotation_y(rotation[1])
+    R = np.dot(R_z, np.dot(R_y, R_x))
+    return R
 
-    # Rotation matrix
-    rotation_matrix = np.dot(y_rotation, x_rotation)
 
-    # Translation vector
+# Function to generate camera extrinsics parameters
+def generate_extrinsics(rotation):
+    roll, pitch, yaw = rotation
+    rotation_matrix = rotation_matrix_from_euler(roll, pitch, yaw)
     translation_vector = np.array([0, 0, 0])
+    pose_matrix = np.eye(4)
+    pose_matrix[:3, :3] = rotation_matrix
+    pose_matrix[:3, 3] = translation_vector
 
-    # Constructing the 4x4 pose matrix
-    pose_matrix = np.eye(4)  # Start with an identity matrix
-    pose_matrix[:3, :3] = rotation_matrix  # Insert rotation
-    pose_matrix[:3, 3] = translation_vector  # Insert translation
+    image_pose = ""
+    for row in pose_matrix:
+        image_pose += " ".join([str(num) for num in row]) + "\n"
+    image_pose = image_pose.encode("utf-8")
 
-    return pose_matrix
+    return image_pose
 
 
-# Function to generate images using SD
 def generate_images(
     prompt,
-    view_scale=1.5,
-    fovy=20,
-    radius=5,
+    fovy=45,
     width=None,
     height=None,
     style=None,
     device="cuda",
     initial_negative_prompt=None,
 ):
-    # Define image dimensions
+    pipe_base, pipe_refiner = build_pipes(device)
+
     if not width:
         width = 768
     if not height:
         height = 768
 
     final_width = final_height = 128
-
-    # Define the image folder
     image_folder = "image_data/"
     util.cond_mkdir(image_folder)
 
@@ -153,7 +151,6 @@ def generate_images(
     util.cond_mkdir(generated_images_folder)
 
     prompt += ", in the center of a blank background"
-    # Add style to prompt
     if style:
         try:
             prompt += ", " + promptStyles.styles[style]
@@ -161,67 +158,59 @@ def generate_images(
             print("Prompt style not found. Defaulting to prompt provided.")
 
     clean_prompt = prompt.replace(" ", "_").replace(".", ",")
-
-    # What we do not want to see
     negative_prompt = (
-        "assymetric measures, unrecognizable distortions, blurry, disfigured, small, not full body, no vibarant colors, not blank background, toy, deformed, blurry, bad anatomy, disfigured, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long neck, long body, ((((mutated hands and fingers)))), (((out of frame))), cartoon, 3d, (disfigured), (bad art), (deformed), (poorly drawn), (extra limbs), strange colours, blurry, boring, sketch, lacklustre, repetitive, cropped, hands"
+        "not photorealistic, assymetric measures, unrecognizable distortions, blurry, disfigured, small, not full body, no vibarant colors, not blank background, toy, not at the center of the image, deformed, blurry, bad anatomy, disfigured, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long neck, long body, ((((mutated hands and fingers)))), (((out of frame))), cartoon, 3d, (disfigured), (bad art), (deformed), (poorly drawn), (extra limbs), strange colours, blurry, boring, sketch, lacklustre, repetitive, cropped, hands"
         + ((", " + initial_negative_prompt) if initial_negative_prompt else "")
     )
 
-    # Generate intrinsics matrix containing focal lengths and center
-    print("Generating intrinsics matrix")
-    intrinsics = generate_intrinsics(view_scale, fovy, width, height)
-    image_intrinsics = " ".join([str(num) for num in intrinsics])
-    # Other parameters by default 0 and 1
-    image_intrinsics += "\n0. 0. 0.\n"
-    image_intrinsics += "1.\n"
-    dimensions = str(width) + " " + str(height) + "\n"
-    image_intrinsics += dimensions
-    image_intrinsics = image_intrinsics.encode("utf-8")
+    poses = [
+        "front",
+        "90 degrees clockwise",
+        "90 degrees counterclockwise",
+        "back",
+        "overhead",
+        "bottom",
+    ]
+    rotations = [
+        (0, 0, 0),
+        (0, 0, 270),
+        (0, 0, 90),
+        (0, 0, 180),
+        (0, 270, 0),
+        (0, 90, 0),
+    ]
 
-    # Pose names and corresponding rotations
-    poses = ["front", "right sideview", "back", "left sideview", "overhead", "bottom"]
-    rotations = [(0, 0), (90, 0), (180, 0), (270, 0), (0, 90), (0, 270)]
-
-    # First image generated
     first_image = None
-
-    # Generate hdf5 file
-    print("Generating hdf5 file")
     hdf5_filename = image_folder + clean_prompt + ".hdf5"
 
     # Check how many images with the same prompt have been generated before
-    num_equal_images = 0
     try:
         with h5py.File(hdf5_filename, "r") as file:
             num_equal_images = len(file.keys())
     except:
-        pass
+        num_equal_images = 0
 
     # Latent generation with a fixed seed so that we ensure that
     # the generated images are similar
     generator = torch.Generator(device=device)
-    # For a random seed, run the code below. The one I am using works well for a single image generation with a blank background 
+    # For a random seed, run the code below. The one I am using works well for a single image generation with a blank background
     # seed = generator.seed()
-    #seed = 9694010081921693614
-    seed = 3087756310308088877
+    seed = 1500722359
     print("Seed used: " + str(seed))
     generator = generator.manual_seed(seed)
 
     img_arrays = []
     pose_arrays = []
-    intrinsics_arrays = []
+    gen_folder_len = len(os.listdir(generated_images_folder))
 
-    # Generate images for each pose
     for i, pose in enumerate(poses):
-        pose_data = generate_pose_data(rotations[i])
-        image_pose = ""
-        for row in pose_data:
-            image_pose += " ".join([str(num) for num in row]) + "\n"
-
         if first_image is None:
             # Include pose in propmpt
-            pose_prompt = prompt + ", viewed from the " + pose + " position"
+            pose_prompt = (
+                prompt
+                + (", viewed  from the " if i < 1 or i > 2 else ", viewed rotated ")
+                + pose
+            )
 
             latents = torch.randn(
                 (1, pipe_base.unet.config.in_channels, height // 8, width // 8),
@@ -267,29 +256,22 @@ def generate_images(
                 latents=latents,
             ).images[0]
 
-        # Save the images in the generated_images folder
-        image.save(
-            generated_images_folder
-            + str(len(os.listdir(generated_images_folder)) + 1)
-            + ".png"
-        )
-
-        # Encode the data
+        gen_folder_len += 1
+        image.save(generated_images_folder + str(gen_folder_len) + ".png")
         image = image.resize((final_width, final_height))
-        img_array = np.array(image)
-        image_pose = image_pose.encode("utf-8")
+        img_array = np.array(image).encode("utf-8")
 
-        # Append the data to the arrays
+        image_pose = generate_extrinsics(rotations[i])
+
         img_arrays.append(img_array)
         pose_arrays.append(image_pose)
-        intrinsics_arrays.append(image_intrinsics)
 
-    # Save the images in the hdf5 file
     with h5py.File(hdf5_filename, "a") as file:
-        # Create a group in the file
         group = file.create_group("instance_" + str(num_equal_images + 1))
 
-        # Create datasets for rgb, pose, intrinsics
+        print("Generating camera intrinsics")
+        image_intrinsics = generate_intrinsics(fovy, width, height)
+
         rgbs_data = group.create_group("rgb")
         poses_data = group.create_group("pose")
         group.create_dataset("intrinsics.txt", data=image_intrinsics)
